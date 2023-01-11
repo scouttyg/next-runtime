1d876141216d31ccfa96291259005de462cdccbc
Matt Kane <m@mk.gg>
2023-01-11 17:11:11 +0000

    feat: use edge function layer for ipx

--- packages/runtime/src/helpers/edge.ts
+++ packages/runtime/src/helpers/edge.ts
@@ -61,6 +61,7 @@ export interface FunctionManifest {
         cache?: 'manual'
       }
   >
+  layers?: Array<{ name: `https://${string}`; flag: string }>
   import_map?: string
 }
 
@@ -304,6 +305,7 @@ export const writeEdgeFunctions = async ({
 }) => {
   const manifest: FunctionManifest = {
     functions: [],
+    layers: [],
     version: 1,
   }
 
@@ -335,7 +337,12 @@ export const writeEdgeFunctions = async ({
     manifest.functions.push({
       function: 'ipx',
       name: 'next/image handler',
-      path: '/_next/image*',
+      path: nextConfig.images.path || '/_next/image',
+    })
+
+    manifest.layers.push({
+      name: 'https://ipx-edge-function-layer.netlify.app/mod.ts',
+      flag: 'ipx-edge-function-layer-url',
     })
   }
   if (!destr(process.env.NEXT_DISABLE_NETLIFY_EDGE)) {
